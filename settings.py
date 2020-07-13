8d90b696d063683b7bb1f46f8809d209b5856944
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2020-07-14 02:50:08 +0530

    reh

--- settings.py
+++ settings.py
@@ -1,16 +1,25 @@
+############################################
+             #BASIC PARAMETERS
+############################################
+
 fps = 0 #How many frames of every second of the video will be extracted. 0 - full fps
-Denoise_factor = 1 #[1-20]
-Batch_size = 16 # 
-Target_Iterations = 500000 # 
-Resolution = 256 # [64-640] More resolution requires more VRAM and time to train. Value will be adjusted to multiple of 16 and 32 for -d archi.
-output_img_ext = "png" # ["png","jpg"] png is lossless, but extraction is x10 slower for HDD, requires x10 more disk space than jpg.
+Resolution = 256 # Face Resolution: [64-640] More resolution requires more VRAM and time to train. Value will be adjusted to multiple of 16 and 32 for -d archi.
 Output_image_Resolution = 512 #[256,2048] Output image size. The higher image size, the worse face-enhancer works. Use higher than 512 value only if the source image is sharp enough and the face does not need to be enhanced
+Face_Type = 'head' #['f','wf','head'] Full face / whole face / head. 'Whole face' covers full area of face include forehead. 'head' covers full head, but requires XSeg for src and dst faceset.
+Batch_size = 16 # If you get memory error reduce the batch size
+Target_Iterations = 500000 # Specify total Iteration  
+max_faces_from_image = 0 #Max number of faces from image:If you extract a src faceset that has frames with a large number of faces, it is advisable to set max faces to 3 to speed up extraction. 0 - unlimited
+output_img_ext = "png" # ["png","jpg"] png is lossless, but extraction is x10 slower for HDD, requires x10 more disk space than jpg.
 jpeg_quality = 90 #[1,100] Jpeg quality. The higher jpeg quality the larger the output file size.
 bitrate = 16 # Bitrate of output video file in MB/s
 output_debug = False #Write debug images
-max_faces_from_image = 0 #Max number of faces from image:If you extract a src faceset that has frames with a large number of faces, it is advisable to set max faces to 3 to speed up extraction. 0 - unlimited
-Face_Type = 'head' #['f','wf','head'] Full face / whole face / head. 'Whole face' covers full area of face include forehead. 'head' covers full head, but requires XSeg for src and dst faceset.
 models_opt_on_gpu = True #Place models and optimizer on GPU
+
+############################################
+            #Training PARAMETERS
+############################################
+
+Denoise_factor = 1 #[1-20]
 ae_dims = 256 #AutoEncoder dimensions: All face information will packed to AE dims. If amount of AE dims are not enough, then for example closed eyes will not be recognized. More dims are better, but require more VRAM. You can fine-tune model size to fit your GPU.
 e_dims = 64 #Encoder dimensions: More dims help to recognize more facial features and achieve sharper result, but require more VRAM. You can fine-tune model size to fit your GPU.
 d_dims = None #Decoder dimensions: More dims help to recognize more facial features and achieve sharper result, but require more VRAM. You can fine-tune model size to fit your GPU.
@@ -26,6 +35,11 @@ bg_style_power = 0.0 #Learn the area outside mask of the predicted face to be th
 ct_mode = 'none' #Color transfer for src faceset: ['none','rct','lct','mkl','idt','sot']. Change color distribution of src samples close to dst samples. Try all modes to find the best.
 clipgrad = False #Gradient clipping reduces chance of model collapse, sacrificing speed of training.
 pretrain = False #Pretrain the model with large amount of various faces. After that, model can be used to train the fakes more quickly.
+
+############################################
+           #Conversion PARAMETERS
+############################################
+
 merging_mode = 1 #(0) original (1) overlay (2) hist-match (3) seamless (4) seamless-hist-match (5) raw-rgb (6) raw-predict
 mask_merging_mode = 2 #(1) dst (2) learned-prd (3) learned-dst (4) learned-prd*learned-dst (5) learned-prd+learned-dst (6) XSeg-prd (7) XSeg-dst (8) XSeg-prd*XSeg-dst (9) learned-prd*learned-dst*XSeg-prd*XSeg-dst
 sharpen_mode = 1 #(0) None (1) box (2) gaussian
